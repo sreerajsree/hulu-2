6c001436ef87994ef27adbc4290861e53e96656f
sreerajsree <sreeandsreeraj@gmail.com>
2022-09-29 17:48:18 +0530

    header

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "links.papareact.com",
+      "image.tmdb.org"
+    ],
+  },
 }
 
 module.exports = nextConfig
