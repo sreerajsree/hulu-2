680656200a039c317bbb722be941373de314bf40
sreerajsree <sreeandsreeraj@gmail.com>
2022-09-30 13:12:24 +0530

    bugss

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -3,7 +3,7 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
 const Thumbnail = forwardRef(({ result }, ref) => {
-  console.log(result);
+
   const BASE_URL = " https://image.tmdb.org/t/p/original/";
   return (
     <div
